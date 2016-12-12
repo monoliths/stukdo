85017cc088e838b651bdce538e2b0742837187c5
Gerald <gerald2557@gmail.com>
2016-12-12 01:44:16 -0800

    added change action to task to update task state

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    # in addition to the crud routes add the following
+    member do
+      put :change
+    end
+  end
   devise_for :users
   get 'about', to: 'pages#about'
   get 'pages/home'
