0bae3c3f15637db06a0844e283ee48c93feb0bad
Gerald <gerald2557@gmail.com>
2016-12-12 21:37:36 -0800

    removed soundcloud from autohtml. not working in production

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,7 +8,6 @@ class Task < ApplicationRecord
   auto_html_for :content do
   	html_escape
   	image
-    soundcloud
   	youtube(width: '100%', height: 250 )
 	  link target: '_blank', rel: 'nofollow'
   	simple_format
