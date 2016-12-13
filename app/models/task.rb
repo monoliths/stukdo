25462adcdc001bc7b19cd0b9be3d15010143ed89
Gerald Hernandez <gerald2557@gmail.com>
2016-12-13 01:07:38 +0000

    added validations for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :content, presence: true
+  
   # prerocessor  to process tasks and generate html for them if applicable
   auto_html_for :content do
   	html_escape
