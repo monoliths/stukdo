5c442c3c2ba81686f1d70a1772a6952ef84695b2
Gerald Hernandez <gerald2557@gmail.com>
2016-12-13 00:10:00 +0000

    updated form styling and added logic after sign in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
+	before_action :redirect_to_tasks, only: [:home, :about]
   def home
   end
   
   def about
+  end
+  
+  private
+
+  def redirect_to_tasks
+  	redirect_to tasks_path if current_user
   end
 end
