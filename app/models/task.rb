a3ac2fb8aad7bead28283e3b1bccab1e6f5a5dfe
Gerald <gerald2557@gmail.com>
2016-12-12 21:03:08 -0800

    added souncloud inegreation

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,13 +3,14 @@ class Task < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true
-  
+
   # prerocessor  to process tasks and generate html for them if applicable
   auto_html_for :content do
   	html_escape
   	image
+    soundcloud
   	youtube(width: '100%', height: 250 )
-	link target: '_blank', rel: 'nofollow' 
+	  link target: '_blank', rel: 'nofollow'
   	simple_format
   end
 end
